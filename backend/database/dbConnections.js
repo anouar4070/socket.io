cdbcbfa975881a0a606f12e03c081d863e312eb6
anouar garbaa <g.anouar@yahoo.com>
2024-08-06 23:37:13 +0100

    starting files

---
+++ backend/database/dbConnections.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+export function dbConnection() {
+  mongoose
+    .connect("mongodb://127.0.0.1:27017/socket")
+    .then(() => {
+      console.log("db connected");
+    })
+    .catch((err) => {
+      console.log("db error" + err);
+    });
+}
